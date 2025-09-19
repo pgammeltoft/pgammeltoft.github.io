f132f6446c4609844bf6e26bec04e16882e8168b
pgammeltoft <peter.gammeltoft@gmail.com>
2025-09-19 23:32:41 +0200

    Add files via upload

---
+++ _vti_cnf/animate.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|Peter
+vti_modifiedby:SR|Peter
+vti_timecreated:TR|18 Jan 2000 00:33:39 -0000
+vti_timelastmodified:TR|18 Jan 2000 00:33:39 -0000
+vti_backlinkinfo:VX|main2.htm main+old+animated.htm
+vti_extenderversion:SR|4.0.2.2717
+vti_syncofs_home14.inet.tele.dk\:21:TW|27 Mar 1999 14:20:00 -0000
+vti_syncwith_home14.inet.tele.dk\:21:TR|17 Jan 2000 23:33:40 -0000
